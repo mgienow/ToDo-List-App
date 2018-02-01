c0ac1d6b268e96bee0d373e34749bc83161964bf
mgienow <michellegienow@gmail.com>
2018-02-01 14:54:22 -0500

    v10.4 event delegation and bubbling

--- v10.js
+++ v10.js
@@ -56,10 +56,9 @@ var handlers = {
     changeTodoTextInput.value = '';
     view.displayTodos();
   },
-  deleteTodo: function() {
-    var deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput');
-    todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
-    deleteTodoPositionInput.value = '';
+  deleteTodo: function(position) {
+
+    todoList.deleteTodo(position);
     view.displayTodos();
   },
   toggleCompleted: function() {
@@ -89,6 +88,7 @@ var view = {
         todoTextWithCompletion = '( ) ' + todo.todoText;
       }
 
+      todoLi.id = i;
       todoLi.textContent = todoTextWithCompletion;
       todoLi.appendChild(this.createDeleteButton());
       todosUl.appendChild(todoLi);
@@ -101,9 +101,40 @@ var view = {
       return deleteButton;
   }
 };
+
+let todosUl = document.querySelector('ul');
+    todosUl.addEventListener('click', function(event){
+      //Get the element that was clicked on:
+    let elementClicked = event.target;
+    //check to make sure it is a delete button:
+      if (elementClicked.className === 'deleteButton'){
+    //run handlers.deleteTodo(position)
+    handlers.deleteTodo(parseInt(elementClicked.parentNode.id));
+      }
+})
 //v10.1: starting at line 95, added createDeletButton method on the view object to
 //have a way for a delete button to appear in the app
 
 //v10.2: line 93, took the todoLi and used appendChild to add the
 //createDeleteButton() method to displayTodos, so that every time a newTodo
 //item is created it will have its own delete buttons
+
+// v10.3: in line 92 (which pushes down our refs to lines 93 & 95 above, lol)
+// we append an id element to todoLi -- todoLi.id -- and set it equal to i
+// so that every time the for loop iterates, i will be unique for each Li created
+
+// EVENT DELEGATION:  
+// v10.4: line 105, establish new todosUl variable so we can grab the Ul
+// from the markup and put an event listener on it. All our todo items are child Li's
+// of the UL, so if they are clicked it registers on the parent node, Ul
+
+//then make sure that what was clicked was a delete button, and not something else in the UL,
+//by creating a var called elementClicked that uses the target attribute assigned
+//by the browser to that element (in control panel) : event.target returns whatever
+//was clilcked
+
+//then if that is true, go to handlers and get the deleteTodo method and pass
+//in as ar elementClicked's parentNode and the id on it
+
+//note: elementClicked.parentNode.id returns a string so you need parseInt()
+//to make sure it's passed as a number to the event handler function
